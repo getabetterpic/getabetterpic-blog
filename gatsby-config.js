93fd583e597858dfcc9ed2dac38303d7d3e22f42
Dan Smith <dan@callrail.com>
2019-03-24 07:57:41 -0400

    Update bio

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: 'An Engineer in Atlanta',
+    author: 'Dan Smith',
+    description: 'Musings from an unordered mind.',
+    siteUrl: `https://getabetterpic.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `getabetterpic`,
     },
   },
   plugins: [
